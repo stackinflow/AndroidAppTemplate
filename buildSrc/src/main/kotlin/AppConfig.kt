2fe5ea3dc85a1e5251411c9901d7d7d6853d85ce
prudhvi reddy <prudhvireddy.m01@gmail.com>
2020-07-24 07:35:25 +0530

    added buildSrc for build management

---
+++ buildSrc/src/main/kotlin/AppConfig.kt
@@ -0,0 +1,12 @@
+package com.stackinflow.androidapptemplate.buildsrc
+
+object AppConfig {
+  const val MIN_SDK = 23
+  const val TARGET_SDK = 29
+  const val COMPILE_SDK = 29
+
+  const val VERSION_CODE = 1
+  const val VERSION_NAME = "1.0"
+
+  const val APP_ID = "com.stackinflow.androidapptemplate"
+}
